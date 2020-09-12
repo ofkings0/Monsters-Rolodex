1606c877ea6835d4d0d7294225439261e0ed9537
ofkings0 <xusihu18@gmail.com>
2020-09-12 17:02:56 +0100

    Updates

---
+++ precache-manifest.00c8d601140e35f00cf8a899af268fcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ccbefc58ae69e4d56cd7cad3bebce7e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "affecb5b5dabcb9175b9",
+    "url": "/monsters-rolodex/static/css/main.daaedfa7.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "affecb5b5dabcb9175b9",
+    "url": "/monsters-rolodex/static/js/main.4fb96c71.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
